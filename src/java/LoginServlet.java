1a9d8a2e2ed4dfd05a804b2ba53330609938028b
Muhammad Fakirullah <88260844+MuhammadFakirullah@users.noreply.github.com>
2023-04-18 10:28:35 +0800

    Add files via upload

---
+++ src/java/LoginServlet.java
@@ -0,0 +1,69 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Cool Tech
+ */
+@WebServlet(urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //work on dopost method
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        //accept username and password from index.html
+        String username = request.getParameter("username");
+        String password = request.getParameter("psw");
+
+        
+        try{
+            //open connection
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/adminacct?useSSL=false","fakirullah","admin");
+            
+            Statement stm = con.createStatement();
+            ResultSet rs = stm.executeQuery("select * from admin where branchName='"+username+"' and password='"+password+"'");
+            
+            if(rs.next()){
+                request.getSession().setAttribute("username",rs.getString("branchName"));
+                request.getSession().setAttribute("branchid", rs.getInt("branchID"));   
+                response.sendRedirect("adminDashboard.jsp");
+            }
+            else{
+                response.sendRedirect("errorResponseAdmin.jsp");
+            }
+            con.close();
+            
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    
+
+}
